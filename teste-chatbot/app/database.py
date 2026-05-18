f6b299024d5c978004379d2c5e0f13974d69b7da
guigneto <guilhermeneto26@hotmail.com>
2026-05-17 23:31:57 -0300

    refatora volumes do docker-compose e ajusta URL do banco de dados

--- teste-chatbot/app/database.py
+++ teste-chatbot/app/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-URL_BANCO = os.getenv("URL_BANCO", "sqlite:///./banco.db")
+URL_BANCO = os.getenv("URL_BANCO", "sqlite:////app/data/banco.db")
 
 motor = create_engine(URL_BANCO, connect_args={"check_same_thread": False})
 SessaoLocal = sessionmaker(autocommit=False, autoflush=False, bind=motor)
